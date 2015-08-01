236e58687922aac30d256f412367f2a0fb61a89d
Michael Kropat <mail@michael.kropat.name>
2015-08-01 13:55:57 +0000

    Query boxes for available commands

---
+++ caniuse.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+main() {
+    while read -r _main_cmd; do
+        if [ -z "$_main_cmd" ]; then
+            echo
+        else
+            printf '%s: %s\n' "$_main_cmd" "$(get_type "$_main_cmd")"
+        fi
+    done <cmds.txt
+}
+
+get_type() {
+    _get_type_output=$(type "$1" 2>/dev/null)
+
+    if [ $? -eq 0 ]; then
+        case "$_get_type_output" in
+            *builtin*|*keyword*) echo builtin ;;
+            */*)                 echo command ;;
+            *)                   echo yes ;;
+        esac
+    else
+        echo no
+    fi
+}
+
+
+main "$@"
